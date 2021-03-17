dbde69cb3c069093f7dbcb7557c57ea60c3d2b18
Jose Antonio <60385205+joseean29@users.noreply.github.com>
2021-03-17 14:11:55 +0100

    add.php

--- src/add.php
+++ src/add.php
@@ -22,9 +22,11 @@ if(isset($_POST['Submit'])) {
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
 	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
 	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+	$apellido1 = mysqli_real_escape_string($mysqli, $_POST['apellido1']);
+	$apellido2 = mysqli_real_escape_string($mysqli, $_POST['apellido2']);
 
 	// checking empty fields
-	if(empty($name) || empty($age) || empty($email)) {
+	if(empty($name) || empty($age) || empty($email) || empty($apellido1) || empty($apellido2)) {
 		if(empty($name)) {
 			echo "<div class='alert alert-danger' role='alert'>Name field is empty</div>";
 		}
@@ -37,14 +39,23 @@ if(isset($_POST['Submit'])) {
 			echo "<div class='alert alert-danger' role='alert'>Email field is empty</div>";
 		}
 
+		if(empty($apellido1)) {
+			echo "<div class='alert alert-danger' role='alert'>Apellido1 field is empty</div>";
+		}
+
+		if(empty($apellido2)) {
+			echo "<div class='alert alert-danger' role='alert'>Apellido2 field is empty</div>";
+		}
+
+
 		// link to the previous page
 		echo "<a href='javascript:self.history.back();' class='btn btn-primary'>Go Back</a>";
 	} else {
 		// if all the fields are filled (not empty)
 
 		// insert data to database
-		$stmt = mysqli_prepare($mysqli, "INSERT INTO users(name,age,email) VALUES(?,?,?)");
-		mysqli_stmt_bind_param($stmt, "sis", $name, $age, $email);
+		$stmt = mysqli_prepare($mysqli, "INSERT INTO users(name, apellido1, apellido2, age, email) VALUES(?,?,?,?,?)");
+		mysqli_stmt_bind_param($stmt, "sssis", $name, $apellido1, $apellido2, $age, $email);
 		mysqli_stmt_execute($stmt);
 		mysqli_stmt_free_result($stmt);
 		mysqli_stmt_close($stmt);
